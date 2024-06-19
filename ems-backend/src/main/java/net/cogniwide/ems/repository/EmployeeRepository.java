8d7854b766d9bc906c6b2bd9468739f1d73f304d
Sriman-COGNIWIDE <sriman.mayandi@cogniwide.com>
2024-06-19 17:41:50 +0530

    Delete ems-backend directory

--- ems-backend/src/main/java/net/cogniwide/ems/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package net.cogniwide.ems.repository;
-
-import net.cogniwide.ems.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository <Employee, Long> {
-
-}
--- ems-backend/src/main/java/net/cogniwide/ems/repository/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package net.cogniwide.ems.repository;
-
-import net.cogniwide.ems.entity.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
