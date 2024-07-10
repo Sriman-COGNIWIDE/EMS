deafbf443c5d3caadf14cc1b704f75543c4807eb
Sriman-COGNIWIDE <sriman.mayandi@cogniwide.com>
2024-07-10 14:23:43 +0530

    Add files via upload

---
+++ src/assets/components/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>404</h1>
+            <p>Page Not Found</p>
+            <a href="/">Go to Home</a>
+        </div>
+    );
+};
+
+export default NotFound;
